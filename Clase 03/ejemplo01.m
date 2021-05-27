ade53528ee08127e757759dac0ffbddd6c7a9c2f
Néstor Aldana <nestor.aldana1@gmail.com>
2021-05-27 12:08:38 -0600

    Ejemplo de secante

--- Clase 03/ejemplo01.m
+++ Clase 03/ejemplo01.m
@@ -1,14 +1,23 @@
-preciosProductos  = [2.0 2.50 1.00] % Vector en dolares
-cantidadProductos = [1 3 4] % cantidad de productos
+% Fizz BUZZ
+% Numero es multiplo de 3 Fiizz
+% Numero es multiplo de 5 Buzz
+% Numero es multiplo de 5 y 3 FizzBuzz
+% Si no cumple solo volver a mostra el numero ingresado
 
-preciosProductos(1)
-preciosProductos(2)
-preciosProductos(3)
-preciosProductos(end)
+numero = input('Ingres un numero: ');
+esMultiplo5 = mod(numero,5) == 0;
+esMultiplo3 = mod(numero,3) == 0;
 
-size(preciosProductos)
-
-M = [ 1 2 3; 4 5 6]
-
-M(1,1)
-M(2,2)
+if ( esMultiplo5 & esMultiplo3) 
+  disp("FizzBuzz")
+else 
+  if( esMultiplo3 )
+    disp("Fizz")
+  else
+    if( esMultiplo5)
+      disp("Buzz")
+    else
+      disp(numero)
+    end
+  end
+end
